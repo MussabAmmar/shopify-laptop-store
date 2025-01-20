ba987555d3740f4dca15a11f125c270868e59a7d
MussabAmmar <mussab.ammar.khan@gmail.com>
2025-01-20 19:54:46 +0500

    eslint fix

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -2,5 +2,7 @@ import { getMenu } from "@/lib/shopify";
 
 export async function Navbar() {
   const menu = await getMenu("next-js-frontend-menu");
-  return <nav></nav>;
+  console.log(menu);
+
+  return <nav>nav</nav>;
 }
